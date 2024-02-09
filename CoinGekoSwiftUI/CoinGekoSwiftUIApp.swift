82c5685116e33a702b96d151251630cc36973700
barufe <belkisarufe@gmail.com>
2024-02-08 20:43:13 -0500

    Initial Commit

---
+++ CoinGekoSwiftUI/CoinGekoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinGekoSwiftUIApp.swift
+//  CoinGekoSwiftUI
+//
+//  Created by Germain Seijas on 8/02/24.
+//
+
+import SwiftUI
+
+@main
+struct CoinGekoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
